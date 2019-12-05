d31228fc31582d06843959cbaec7c270af5b8249
Isabelle Langrock <isabellelangrock@levy-ve703-09281.apn.wlan.private.upenn.edu>
2019-12-05 16:48:10 -0500

    Adding new data and data analysis file

--- wikipedia_api/wikipedia.py
+++ wikipedia_api/wikipedia.py
@@ -4,20 +4,36 @@
 #	3. Open ipython with ipython
 #	4. Copy code and paste into ipython with %paste
 
+# Needed Libraries 
 import wptools
+	# This is the python wrapper used for collecting Wikipedia data 
 import pandas as pd
 import csv
 
-# open a csv file called results CHANGE NAME WHEN RUNNING SO IT DOESN'T OVERWRITE
-f = open("artandfemWiki3.csv", 'w')
+#Functions  
+# Net - builds a network structure from a list within a dataframe 
+def net(x):
+	row= x.wikilinks
+	destination= row.split(",")
+	source = x.title
+	subdf=pd.DataFrame(destination)
+	subdf['source']= source 
+	return subdf
+
+#Artists
+	# Downloading Artists Information from list provided by Art+Feminism Dashboard, same process applied 
+	# Scientists. [See below for exact code]
+
+	# First open a csv file for the results  -- WILL OVERWRITE, so change name as needed. 
+artists = open("artandfemWiki3.csv", 'w')
 writer = csv.writer(f)
-labels = ["title",'infobox','length','wikilinks','editorcount','pageID','aveviews', 'categories']
+	# labels for desired categories, see Descriptions in DataCollection.Rmd
+labels = ["title",'infobox','length','wikilinks','editorcount','pageID','aveviews', 'categories'] 
 writer.writerow(labels)
 
-## Sample pages for making sure code works before running on the whole list. 
-articles = ['Donna Strickland', 'Nathan Ransom Leonard ', 'Bill Murray']
 
-# making an array from data names 
+	# Second, make an array with all the names in the (Cleaned up version) of the Art+Feminism edited 
+	# articles document. 
 names = [ ] 
 
 with open('artplusfeminismList_clean.csv') as csvDataFile: 
@@ -26,16 +42,15 @@ with open('artplusfeminismList_clean.csv') as csvDataFile:
 		names.append(row[0])
 print(names)
 
-# adding data for each article 
+	# Collect data for each identified article in the list with a for loop. Exclude any that do not have 
+	# pages (For more information see DataCollection.Rmd)
 for a in names:
 	# fetch page data
-	#	to see what's available, run  page.data.keys() 
 	try: 
 		page = wptools.page(a)
 		page.get_parse()
 		page.get_query()
 		page.get_more()
-
 		# pull out page data into variables
 		title = page.data['title']
 		infobox = page.data['infobox']
@@ -46,18 +61,76 @@ for a in names:
 		ID = page.data['pageid']
 		ave_views = page.data['views']
 		categories = page.data['categories']
-
 		# create an array called row with the variables
 		row = [title, infobox, pagelength, wikilinks, editors, ID, ave_views, categories]
 		print(row)
-
 		# write that row to a csv
 		writer.writerow(row)
 	except:
 		print("Page does not exist")
-f.close()
+artists.close()  #closes the called for .csv above 
 
-#Getting Data about politicians - first pulling names: 
+	# Pull out the wikilink networks 
+Artists = pd.read_csv("Data_Sets/artandfemWiki.csv")
+
+WikiNet = Artists.apply(net, axis=1)
+WikiNet = list(WikiNet)
+Network = pd.concat(WikiNet)
+Network.columns=["Destination","Source"]
+
+print(Network.head())
+
+Network.to_csv("Wikilinks_Networks/ArtistsNet.csv")
+
+#Scientists
+
+scientists = open("scientistswiki.csv", 'w')
+writer = csv.writer(f)
+labels = ["title",'infobox','length','wikilinks','editorcount','pageID','aveviews', 'categories'] 
+writer.writerow(labels)
+
+names = [ ] 
+
+with open('Raw_Data/500WomenScientistsList.csv') as csvDataFile: 
+	csvReader = csv.reader(csvDataFile)
+	for row in csvReader:
+		names.append(row[0])
+print(names)
+
+for a in names:
+	try: 
+		page = wptools.page(a)
+		page.get_parse()
+		page.get_query()
+		page.get_more()
+		title = page.data['title']
+		infobox = page.data['infobox']
+		pagelength = page.data['length']
+		wikilinks = page.data['links']
+		editors = page.data['contributors']
+		ID = page.data['pageid']
+		ave_views = page.data['views']
+		categories = page.data['categories']
+		row = [title, infobox, pagelength, wikilinks, editors, ID, ave_views, categories]
+		print(row)
+		writer.writerow(row)
+	except:
+		print("Page does not exist")
+scientists.close()  
+
+Scientists = pd.read_csv("Data_Sets/ScientistsWiki.csv")
+
+WikiNet = Scientists.apply(net, axis=1)
+WikiNet = list(WikiNet)
+Network = pd.concat(WikiNet)
+Network.columns=["Destination","Source"]
+
+print(Network.head())
+
+Network.to_csv("Wikilinks_Networks/ScientistsNet.csv")
+
+
+#Politicians
 
 sources = ['List of female United States presidential and vice-presidential candidates', 
 			'List of female governors in the United States',
@@ -95,14 +168,7 @@ for n in sources:
 		print("PageDoesNotExist")
 politics.close
 
-femalepoliticians = pd.read_csv("politicians.csv")
 
-WikiNet = femalepoliticians.apply(net, axis=1)
-WikiNet = list(WikiNet)
-Network = pd.concat(WikiNet)
-Network.columns=['Destination', 'Source']
-
-Network.to_csv("FemalePoliticians.csv")
 
 # First Manual Cleanup -- remove all none humans, then return to download information 
 
@@ -113,7 +179,7 @@ writer.writerow(labels)
 
 names = [ ]
 
-with open('FemalePoliticians.csv') as csvDataFile: 
+with open('Raw_Data/FemalePoliticians.csv') as csvDataFile: 
 	csvReader = csv.reader(csvDataFile)
 	for row in csvReader:
 		names.append(row[0])
@@ -148,6 +214,15 @@ for a in names:
 	except:
 		print("Page does not exist")
 p.close()
+
+femalepoliticians = pd.read_csv("Data_Sets/politicsWiki.csv")
+
+WikiNet = femalepoliticians.apply(net, axis=1)
+WikiNet = list(WikiNet)
+Network = pd.concat(WikiNet)
+Network.columns=['Destination', 'Source']
+
+Network.to_csv("Wikilinks_Networks/PoliticiansNet.csv")
 
 #Locating page information for athletes 
 
@@ -190,6 +265,7 @@ Network = pd.concat(WikiNet)
 Network.columns=['Destination', 'Source']
 
 Network.to_csv("FemaleAthletes.csv")
+
 #Manual Cleanup 
 
 p = open("athletesWiki.csv", 'w')
@@ -236,9 +312,18 @@ for a in names:
 
 p.close()
 
+femaleathletes = pd.read_csv("Data_Sets/athletesWiki.csv")
+
+WikiNet = femaleathletes.apply(net, axis=1)
+WikiNet = list(WikiNet)
+Network = pd.concat(WikiNet)
+Network.columns=['Destination', 'Source']
+
+Network.to_csv("Wikilinks_Networks/AthletesNet.csv")
 
 
 # How to extract network information 
+# Creating a function, for dividing into networks. 
 
 def net(x):
 	row= x.wikilinks
@@ -247,7 +332,7 @@ def net(x):
 	subdf=pd.DataFrame(destination)
 	subdf['source']= source 
 	return subdf
-
+# Sample 
 sample = pd.read_csv("sample.csv")
 
 WikiNet = sample.apply(net, axis=1)
@@ -258,6 +343,7 @@ Network.columns=["Destination","Source"]
 print(Network.head())
 
 Network.to_csv("SampleNet.csv")
+
 
 
 
